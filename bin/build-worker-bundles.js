eb8604a00eafa9aca10addbc3815dca8cbed7cdd
Jeremy Scheff <jdscheff@gmail.com>
2019-11-06 09:08:40 -0500

    Simplify

--- bin/build-worker-bundles.js
+++ bin/build-worker-bundles.js
@@ -1,15 +1,10 @@
 const glob = require("glob");
-const mkdirp = require("mkdirp");
 const path = require("path");
-const rimraf = require("rimraf");
 const rollup = require("rollup");
 const babel = require("rollup-plugin-babel");
 const commonjs = require("rollup-plugin-commonjs");
 const resolve = require("rollup-plugin-node-resolve");
 const builtins = require("rollup-plugin-node-builtins");
-
-rimraf.sync("test/bundle");
-mkdirp.sync("test/bundle");
 
 const files = glob.sync("test/worker*js");
 
@@ -28,7 +23,7 @@ Promise.all(
     });
 
     await bundle.write({
-      file: `test/bundle/bundle-${path.basename(file)}`,
+      file: `dist/test/bundle/bundle-${path.basename(file)}`,
       format: "iife",
       indent: false,
       name: "test"
